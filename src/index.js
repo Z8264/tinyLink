0d2adbe21d96d1a170020acd778d967b428ee2ac
张博 <zhangbo@panda.tv>
2018-09-21 17:31:37 +0800

    Signed-off-by: 张博 <zhangbo@panda.tv>

--- src/index.js
+++ src/index.js
@@ -3,33 +3,84 @@ import "./less/all.less";
 import data from "./data";
 
 const tpl = (data)=>`
-  <ul>
-    <li>
-      <a href="">
-        <span class="icon"><span>
-        <span>name</span>
-      </a>
-    </li>
-    <li>
-      <a href="">
-        <span class="icon"><span>
-        <span class="name">百度</span>
-        <span class="url">baidu.com</span>
-      </a>
-    </li>
-    <li>
-      <a href="">
-        <span class="icon"><span>
-        <span>name</span>
-      </a>
-    </li>
-    <li>
-      <a href="">
-        <span class="icon"><span>
-        <span>name</span>
-      </a>
-    </li>
-  </ul>
+<div class="main">
+  <section class="column">
+    <div class="link layout-gird">
+      <div class="link-head">
+        <h2>标题</h2>
+        <a href="">setting</a>
+      </div>
+      <ul class="link-list clearfix">
+        <li>
+          <a href="">
+            <span class="icon"></span>
+            <span class="name">百度</span>
+            <span class="url">baidu.com</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <span class="icon"></span>
+            <span class="name">网址</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <span class="icon"></span>
+            <span class="name">网址</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <span class="icon"></span>
+            <span class="name">网址</span>
+          </a>
+        </li>
+      </ul>
+      <div class="link-foot">
+      </div>
+    </div>
+  </section>
+  <section class="column">
+    <div class="link layout-list">
+      <div class="link-head">
+        <h2>标题</h2>
+        <a href="">setting</a>
+      </div>
+      <ul class="link-list clearfix">
+        <li>
+          <a href="">
+            <span class="icon"></span>
+            <span class="name">百度</span>
+            <span class="url">baidu.com</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <span class="icon"></span>
+            <span class="name">网址</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <span class="icon"></span>
+            <span class="name">网址</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <span class="icon"></span>
+            <span class="name">网址</span>
+          </a>
+        </li>
+      </ul>
+      <div class="link-foot">
+      </div>
+    </div>
+  </section>
+  <section class="column">
+  </section>
+</div>
 `
 
 function init() {
